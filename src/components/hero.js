521bb4be3f410e96f583424799b546f2cb8bab1d
Kalbz <kalle1942@live.se>
2025-07-14 05:42:57 +0200

    Messed around with blobs on hero, and added new layouts for hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,50 +6,145 @@ export function createHero() {
 
   const { stockUrl, triUrl } = randomImage();
 
-  const sizeOptions = ['max-w-sm','max-w-md','max-w-lg','max-w-xl','w-full','lg:w-1/2'];
-  const imgSize = sizeOptions[Math.floor(Math.random() * sizeOptions.length)];
+  const sizeOptions = ['max-w-sm', 'max-w-md', 'max-w-lg', 'max-w-xl', 'w-full', 'lg:w-1/2'];
+  const biggerSizeOptions = [
+    'w-full max-w-3xl',
+    'w-full max-w-5xl',
+    'w-full max-w-6xl',
+    'w-[90%]',
+    'w-[95%]',
+    'lg:w-[75%]'
+  ];
 
-    const image = Math.random() < 0.5 ? stockUrl : triUrl;
+  const imgBiggerSize = biggerSizeOptions[Math.floor(Math.random() * biggerSizeOptions.length)];
+  const imgSize = sizeOptions[Math.floor(Math.random() * sizeOptions.length)];
+  const image = Math.random() < 0.5 ? stockUrl : triUrl;
 
 
   const templates = [
-    `
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1 class="text-5xl font-bold">Dynamic Hero</h1>
-        <p class="py-6">This hero got a random background pattern and a random stock photo size.</p>
-        <button class="btn btn-primary">Let’s Go!</button>
-      </div>
-    </div>
-    `,
-    `
-    <div class="hero-content flex-col lg:flex-row items-center">
+    // `
+    // <div class="hero-content text-center">
+    //   <div class="max-w-md">
+    //     <h1 class="text-5xl font-bold">Dynamic Hero</h1>
+    //     <p class="py-6">This hero got a random background pattern and a random stock photo size.</p>
+    //     <button class="btn btn-primary">Let’s Go!</button>
+    //   </div>
+    // </div>
+    // `,
+
+    // ==============================================================
+
+    // `
+    // <div class="hero-content flex-col lg:flex-row items-center">
+    //   <img
+    //     src="${image}"
+    //     alt="Random stock"
+    //     class="${imgSize} rounded-lg shadow-2xl mb-4 lg:mb-0"
+    //   />
+    //   <div class="lg:pl-6">
+    //     <h1 class="text-5xl font-bold">Box Office News!</h1>
+    //     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi.</p>
+    //     <button class="btn btn-primary">Get Started</button>
+    //   </div>
+    //         <img
+    //     src="${image}"
+    //     alt="Random stock"
+    //     class="${imgSize} rounded-lg shadow-2xl mb-4 lg:mb-0"
+    //   />
+    // </div>
+    // `,
+// ==============================================================
+      `
+<div class="hero-content flex flex-row justify-center items-center gap-6 w-full">
       <img
         src="${image}"
         alt="Random stock"
-        class="${imgSize} rounded-lg shadow-2xl mb-4 lg:mb-0"
+        class="${imgSize} rounded-lg shadow-2xl"
       />
-      <div class="lg:pl-6">
-        <h1 class="text-5xl font-bold">Box Office News!</h1>
-        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi.</p>
-        <button class="btn btn-primary">Get Started</button>
-      </div>
-    </div>
-    `,
-    `
-    <div class="hero-content flex-col lg:flex-row-reverse items-center">
+
       <img
         src="${image}"
         alt="Random stock"
-        class="${imgSize} rounded-lg shadow-2xl mb-4 lg:mb-0"
+        class="${imgBiggerSize} rounded-lg shadow-2xl "
       />
-      <div class="lg:pr-6">
-        <h1 class="text-5xl font-bold">Box Office News!</h1>
-        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi.</p>
-        <button class="btn btn-primary">Get Started</button>
-      </div>
+
+      <img
+        src="${image}"
+        alt="Random stock"
+        class="${imgSize} rounded-lg shadow-2xl"
+      />
     </div>
+
+    
+    `,
+
     `
+    <div class="hero-content flex flex-wrap justify-center items-center gap-4">
+      <img
+        src="${image}"
+        alt="Random stock"
+        class="${imgSize} rounded-lg shadow-2xl"
+      />
+
+      <img
+        src="${image}"
+        alt="Random stock"
+        class="${imgBiggerSize} rounded-lg shadow-2xl"
+      />
+
+      <img
+        src="${image}"
+        alt="Random stock"
+        class="${imgSize} rounded-lg shadow-2xl"
+      />
+    </div>
+    `,
+
+    // ==============================================================
+
+    // `
+    // <div class="hero-content flex-col lg:flex-row-reverse items-center">
+    //   <img
+    //     src="${image}"
+    //     alt="Random stock"
+    //     class="${imgSize} rounded-lg shadow-2xl mb-4 lg:mb-0"
+    //   />
+    //   <div class="lg:pr-6">
+    //     <h1 class="text-5xl font-bold">Box Office News!</h1>
+    //     <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi.</p>
+    //     <button class="btn btn-primary">Get Started</button>
+    //   </div>
+    // </div>
+    // `,
+//     `
+
+// ==============================================================
+
+
+// <div class="absolute inset-0 z-0 pointer-events-none overflow-visible">
+//   <svg viewBox="0 0 1000 1000" xmlns="http://www.w3.org/2000/svg"
+//     <defs>
+//       <linearGradient id="gradient" x1="0%" y1="0%" x2="0%" y2="100%">
+//         <stop offset="0%" style="stop-color: rgb(238, 205, 163);"></stop>
+//         <stop offset="100%" style="stop-color: rgb(239, 98, 51);"></stop>
+//       </linearGradient>
+//     </defs>
+//     <path d="M457.5,323Q424,396,357,436.5Q290,477,208,471Q126,465,79.5,397Q33,329,34,250.5Q35,172,84,109.5Q133,47,212.5,28.5Q292,10,363,53Q434,96,462.5,173Q491,250,457.5,323Z"
+//           fill="url(#gradient)"></path>
+//   </svg>
+// </div>
+
+//   <div class="max-w-xl text-center lg:text-left z-10">
+//     <h1 class="text-5xl font-bold">Hero with SVG Blob</h1>
+//     <p class="py-6">This version has a background SVG gradient blob layered behind the content.</p>
+//     <button class="btn btn-primary">Let’s Go!</button>
+
+// </div>
+// `
+
+// ==============================================================
+
+
   ];
 
   hero.innerHTML = templates[Math.floor(Math.random() * templates.length)];
@@ -57,3 +152,5 @@ export function createHero() {
 
   return hero;
 }
+
+
--- src/layouts/layoutTemplate.js
+++ src/layouts/layoutTemplate.js
@@ -12,35 +12,42 @@
 //   // ['Navbar', 'Hero', 'Carousel', 'Avatar', 'Boxes', 'Footer', 'Card', 'Accordion', 'Table', 'Comparison', 'Divider'],
 // ];
 
-export const layoutTemplates = [
-  [
 
-// { type: "Spacer", layout: { direction: "vertical", size: "48" } },
-// =================================
-// {
-//   type: "Text",
-//   props: { size: "text-5xl", align: "center" },
-//   layout: { padding: "py-8" }
-// }
+// COMMENT HERE
+// export const layoutTemplates = [
+//   [
+
+// // { type: "Spacer", layout: { direction: "vertical", size: "48" } },
+// // =================================
+// // {
+// //   type: "Text",
+// //   props: { size: "text-5xl", align: "center" },
+// //   layout: { padding: "py-8" }
+// // }
+// // ,
+
+// //     { type: "Avatar", layout: { align: "center", padding: "pt-8 pb-8", background: "bg-base-200" } },
+// //     { type: "LongText", layout: { align: "center", padding: "pt-8", background: "bg-base-100" } },
+
+// //   ],
+
+// // =================================
+// { type: "Spacer", layout: { direction: "vertical", size: "32" } },
+
 // ,
 
 //     { type: "Avatar", layout: { align: "center", padding: "pt-8 pb-8", background: "bg-base-200" } },
-//     { type: "LongText", layout: { align: "center", padding: "pt-8", background: "bg-base-100" } },
+//     {
+//   type: "Text",
+//   props: { size: "text-5xl", align: "center" },
+//   layout: { padding: "py-0" }
+// },
+//     { type: "LongText", layout: { align: "center", padding: "pt-0", background: "bg-base-100" } },
 
 //   ],
+// ];
 
-// =================================
-{ type: "Spacer", layout: { direction: "vertical", size: "32" } },
 
-,
-
-    { type: "Avatar", layout: { align: "center", padding: "pt-8 pb-8", background: "bg-base-200" } },
-    {
-  type: "Text",
-  props: { size: "text-5xl", align: "center" },
-  layout: { padding: "py-8" }
-},
-    { type: "LongText", layout: { align: "center", padding: "pt-8", background: "bg-base-100" } },
-
-  ],
-];
+export const layoutTemplates = [
+  ['Hero']
+]
